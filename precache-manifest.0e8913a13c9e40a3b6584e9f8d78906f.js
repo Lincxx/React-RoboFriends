a5e8f0a5d794fac5917daf12d5bf701c6e246f75
Lincxx <bossco28@gmail.com>
2020-05-26 12:19:33 -0400

    Updates

---
+++ precache-manifest.0e8913a13c9e40a3b6584e9f8d78906f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e4a10ec5346a9168a056c0c30fe8ca",
+    "url": "/React-RoboFriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/React-RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ed86a3651009cc5ea235",
+    "url": "/React-RoboFriends/static/css/main.9080c952.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/React-RoboFriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-RoboFriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed86a3651009cc5ea235",
+    "url": "/React-RoboFriends/static/js/main.b7521c46.chunk.js"
+  },
+  {
+    "revision": "d68f41e518295a69c1f4",
+    "url": "/React-RoboFriends/static/js/runtime-main.1f3b2bfb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
